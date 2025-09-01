b503d5b7508f9b3fc054c40ad9e2d9398f4e8982
Gm Dev <gmmoose17@gmail.com>
2025-09-02 03:23:38 +0800

    feat: add Book constructor and array

---
+++ javascript-projects/Library/script.js
@@ -0,0 +1,22 @@
+const myLibrary = [
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+
+  },
+  {
+    title: "harry Potter and the Chamber of Secrets",
+  }
+
+];
+
+function Book() {
+  // the constructor...
+  this.title = title;
+
+}
+
+function addBookToLibrary(book) {
+  // take parameter, create a book then store it in the myLibrary array
+  myLibrary.push(book);
+
+}
